bea427f817275bccf7ba2bddaab11db08ae9ee92
Jem256 <jemynagasha@gmail.com>
2022-07-19 14:34:52 +0300

    setting up routing and home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Services from './components/services/Services';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Contact from './components/contact/Contact';
+// import Nav from './components/nav/Nav';
+// import About from './components/about/About';
+// import Experience from './components/experience/Experience';
+// import Portfolio from './components/portfolio/Portfolio';
+// import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Nav/>
-      <About/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Header/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+      
+      {/* <Nav/> */}
+      {/* <About/>
       <Experience/>
-      {/* <Services/> */}
       <Portfolio/>
-      {/* <Testimonials/> */}
-      <Contact/>
-      <Footer/>
+      <Contact/> */}
     </div>
   );
 }
