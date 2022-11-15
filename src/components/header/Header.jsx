5b8e08d69d4a66694e0415b21fe23f90328f4c39
Jemimah <jemynagasha@gmail.com>
2022-11-15 16:45:21 +0300

    cv update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,8 @@
-/* eslint-disable react/no-unescaped-entities */
-import React from "react";
-import "./Header.css";
-import { IoIosArrowDown } from "react-icons/io";
-import CTA from "./CTA";
-import HeaderSocials from "./HeaderSocials";
+import React from 'react';
+import './Header.css';
+import { IoIosArrowDown } from 'react-icons/io';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
 
 function Header() {
   return (
@@ -12,22 +11,14 @@ function Header() {
         <h2 className="greet">Hello I'm</h2>
         <h1>Nagasha Jemimah</h1>
         <h2 className="text-light">
-          A web developer based in
-          {" "}
-          <br />
-          {" "}
-          Kampala, Uganda. I love building
-          beautiful interfaces,
-          {" "}
-          <br />
-          {" "}
-          web apps, and everything in between!
+          A web developer based in <br /> Kampala, Uganda. I love building
+          beautiful interfaces, <br /> web apps, and everything in between!
         </h2>
         <CTA />
         <HeaderSocials />
         <div className="center-con">
           <div className="round">
-            <a href="#about" className="cta-arrow bounceAlpha">
+            <a href="/" className="cta-arrow bounceAlpha">
               <IoIosArrowDown className="arrow primera font-semibold" />
               <IoIosArrowDown className="arrow segunda font-semibold" />
             </a>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,71 +1,67 @@
 import React from "react";
 import "./Portfolio.css";
 import IMG1 from "../../assets/ecoOptions.png";
-import IMG2 from "../../assets/linked.png";
-import IMG3 from "../../assets/covid.png";
+import IMG2 from '../../assets/vfarm.png';
+import IMG3 from '../../assets/covid.png';
 
 function Portfolio() {
   const data = [
     {
       id: 1,
-      image: IMG1,
-      title: "Eco Options",
+      image: IMG2,
+      title: 'Virtual Farm',
       description: (
         <p>
-          I built this project as a challenge for myself to apply the
-          <span className="underline"> ReactJs </span>
-          and
-          <span className="underline"> Redux </span>
-          concepts i had learned and also give our budding sustainable menstrual
-          health Ecommerce business a website.
+          I am building this platform for a local startup using{' '}
+          <span className="underline">ReactJs</span> ,{' '}
+          <span className="underline">Context API</span> for state management
+          and <span className="underline">TailwindCss</span> for styling. It
+          uses <span className="underline">Firebase</span> for the backend and
+          hosting.
         </p>
       ),
-      github: "https://github.com/Jem256/ecooptions",
-      demo: "https://ecooptionsug.com",
+      github: 'Private',
+      demo: 'https://v-farm-4653d.web.app/',
     },
     {
       id: 2,
-      image: IMG3,
-      title: "Nigeria Covid Data",
+      image: IMG1,
+      title: 'Eco Options',
       description: (
         <p>
-          This project was built as part of an interview process. 
-          It uses <span className="underline">redux</span> to fetch covid data of nigeria from
-          {" "}
-          <a href="https://covidnigeria.herokuapp.com/api">
-            an open source api.
-          </a>
-          {" "}
-          The UI was built using ReactJs and CSS. A summary of the country's data is displayed and the search feature is used to query data by state.
+          I built this project for a local ecommerce sustainable menstrual
+          health startup,{' '}
+          <a href="https://covidnigeria.herokuapp.com/api">Eco Options.</a>{' '}
+          <span className="underline">ReactJs</span> and the
+          <span className="underline"> Redux ToolKit</span> was used to build
+          and handle state management. It is integrated with{' '}
+          <span className="underline">Flutterwave API</span> to handle customer
+          payments and uses <span className="underline">Firebase</span> for it's
+          backend and hosting.
         </p>
       ),
-      github: "https://github.com/Jem256/FrontendDevTask-FetchData",
-      demo: "https://fetchcoviddata.netlify.app/",
+      github: 'https://github.com/Jem256/ecooptions',
+      demo: 'https://ecooptionsug.com',
     },
     {
       id: 3,
-      image: IMG2,
-      title: "Social Networking App",
+      image: IMG3,
+      title: 'Nigeria Covid Data',
       description: (
         <p>
-          I built this LinkedIn clone from a
-          {" "}
-          <a href="https://www.youtube.com/watch?v=QaYts9sPmcY&t=1s">
-            Youtube Tutorial
-          </a>
-          {" "}
-          while learning
-          <span className="underline"> ReactJs </span>
-          and
-          <span className="underline"> Redux </span>
-          concepts. It
-          {" "}
-          <span className="underline"> Firebase </span>
-          for authentication, database management and hosting.
+          This project was built as part of an interview process. It uses{' '}
+          <span className="underline">redux</span> to fetch covid data of
+          nigeria from{' '}
+          <a href="https://covidnigeria.herokuapp.com/api">
+            an open source api.
+          </a>{' '}
+          The UI was built using ReactJs and CSS. A summary of the country's
+          data is displayed and the search feature is used to query data by
+          state.
         </p>
       ),
-      github: "https://github.com/Jem256/linkedinclone",
-      demo: "https://linkedin-clone-new-58e07.firebaseapp.com/",
+      github: 'https://github.com/Jem256/FrontendDevTask-FetchData',
+      demo: 'https://fetchcoviddata.netlify.app/',
     },
   ];
 
@@ -75,25 +71,35 @@ function Portfolio() {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {
-          data.map(({
-            id, image, title, github, demo, description,
-          }) => (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} width={50} height={300} />
+        {data.map(({ id, image, title, github, demo, description }) => (
+          <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} width={50} height={300} />
+            </div>
+            <div className="portfolio__content">
+              <h3>{title}</h3>
+              {description}
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
               </div>
-              <div className="portfolio__content">
-                <h3>{title}</h3>
-                {description}
-                <div className="portfolio__item-cta">
-                  <a href={github} className="btn btn-primary" target="_blank" rel="noreferrer">Github</a>
-                  <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-                </div>
-              </div>
-            </article>
-          ))
-        }
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
