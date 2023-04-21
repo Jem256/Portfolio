b606c1ad7ca8657c377c1c229c192889ed91e4d5
Jemimah Nagasha <jemynagasha@gmail.com>
2023-04-21 11:25:13 +0300

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/NagashaJ-CV.pdf';
 function CTA() {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href='https://docs.google.com/document/d/1Tvw7k68q0JNNHxigEBnp3rJDdeC-eZkUUYLkvH5tmaw/edit?usp=sharing' className="btn" target="_blank" rel="noopener noreferrer">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
